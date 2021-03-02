d5c2d31513fc20660ff039b4732fe235d152eb6f
Jules Françoise <me@julesfrancoise.com>
2021-03-02 13:20:32 +0100

    Update app generator for webpack

--- generators/app/templates/webpack/webpack.config.js
+++ generators/app/templates/webpack/webpack.config.js
@@ -20,9 +20,6 @@ module.exports = {
     filename: '[name].js',
     chunkFilename: '[name].[id].js',
   },
-  externals: {
-    'chart.js': 'Chart',
-  },
   module: {
     rules: [
       <% if (isTypeScript) { %>{
